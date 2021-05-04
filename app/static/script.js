c07f42698004cea9cacdb055bb5404dace4e259a
Doreen Gui <dg497@cornell.edu>
2021-05-04 14:46:50 -0400

    clear inputs

--- app/static/script.js
+++ app/static/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $("select#search_cat").selectize({
-        plugins: ["remove_button"],
-        delimiter: ",",
-        maxItems: 5,
-        persist: false,
-        create: function (input) {
-        return {
-            value: input,
-            text: input,
-        };
-        },
-    });
-});
+// $(document).ready(function(){
+//     $("select#search_cat").selectize({
+//         plugins: ["remove_button"],
+//         delimiter: ",",
+//         maxItems: 5,
+//         persist: false,
+//         create: function (input) {
+//         return {
+//             value: input,
+//             text: input,
+//         };
+//         },
+//     });
+// });
